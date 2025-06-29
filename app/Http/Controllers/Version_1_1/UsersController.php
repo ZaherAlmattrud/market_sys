3d47944f9aabb9819b8c0836934fe41ea10507fc
ZaherAlmattrud <zaheralmattrud@gmail.com>
2025-06-29 14:54:08 +0300

    .

--- app/Http/Controllers/Version_1_1/UsersController.php
+++ app/Http/Controllers/Version_1_1/UsersController.php
@@ -29,7 +29,7 @@ class UsersController extends Controller
 
     if ($search) {
         $query->where(function ($q) use ($search) {
-            $q->where('user_name', 'like', "%{$search}%")
+            $q->where('user_name', 'like', "%{$search}%");
               
         });
     }
